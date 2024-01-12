4f366513484daa68f10c2b4139318bebe305b297
JongHun Lee <jonghun@JongHunui-MacBookAir.local>
2024-01-12 17:21:50 +0900

    Checkbox Design System 수정

--- lib/design_system/checkbox/checkbox.dart
+++ lib/design_system/checkbox/checkbox.dart
@@ -1,28 +1,38 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/design_system/text/text.dart';
 import 'package:flutter_application_1/design_system/text/text_style.dart';
 
 // ignore: must_be_immutable
 class SCCheckbox extends StatelessWidget {
-  SCCheckbox({
-    super.key,
-    required this.isChecked,
-    required this.onChanged,
-    required this.tristate,
-    this.toplabel,
-    this.rightlabel,
-    this.checkColor,
-  });
+  SCCheckbox(
+      {super.key,
+      required this.isChecked,
+      required this.onChanged,
+      required this.tristate,
+      this.toplabel,
+      this.rightlabel,
+      this.checkColor,
+      this.fillColor,
+      this.activeColor = Colors.red,
+      this.toplabelStyle = SCTextStyle.font_800_12px_100pc_P_black,
+      this.rightlableStyle = SCTextStyle.font_600_12px_100pc_P_black
+});
 
   bool? isChecked;
   String? toplabel;
   String? rightlabel;
   Color? checkColor;
+  Color? fillColor;
+  Color? activeColor;
+  SCTextStyle toplabelStyle;
+  SCTextStyle? rightlableStyle;
+  
   // 체크박스 상태에 - 존재여부
   // true -> - 존재 (value에 null 이 존재함)
   // false -> - 존재 (value에 null 존재하지 않음)
-
   bool tristate;
+
   void Function(bool?)? onChanged;
   @override
   Widget build(BuildContext context) {
@@ -34,8 +44,8 @@ class SCCheckbox extends StatelessWidget {
           children: [
             if (toplabel != null)
               SCText(toplabel!,
-                  textStyle: SCTextStyle.font_800_12px_100pc_P_black),
-            const SizedBox(
+                  textStyle: toplabelStyle),
+            SizedBox(
               height: 4,
             ),
             Row(mainAxisSize: MainAxisSize.min, children: [
@@ -46,10 +56,11 @@ class SCCheckbox extends StatelessWidget {
                   horizontal: VisualDensity.minimumDensity,
                   vertical: VisualDensity.minimumDensity,
                 ),
-                activeColor: Colors.red,
+                activeColor: activeColor,
                 value: isChecked,
                 onChanged: onChanged,
                 checkColor: checkColor,
+                fillColor: MaterialStatePropertyAll(fillColor),
               ),
               if (rightlabel != null)
                 const SizedBox(
@@ -71,10 +82,11 @@ class SCCheckbox extends StatelessWidget {
         horizontal: VisualDensity.minimumDensity,
         vertical: VisualDensity.minimumDensity,
       ),
-      activeColor: Colors.red,
+      activeColor: activeColor,
       value: isChecked,
       onChanged: onChanged,
       checkColor: checkColor,
+      fillColor: MaterialStatePropertyAll(fillColor),
     );
   }
 }
