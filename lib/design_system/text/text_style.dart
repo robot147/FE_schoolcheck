48a87fe8160f4822c5550dc79f8743629bd70ecd
8848man <83057037+8848man@users.noreply.github.com>
2024-01-18 17:29:19 +0900

    -- 최신 dev 브랜치 덮어쓰기

--- lib/design_system/text/text_style.dart
+++ lib/design_system/text/text_style.dart
@@ -1,1 +1,490 @@
+// ignore_for_file: constant_identifier_names
 
+import 'package:flutter/widgets.dart';
+
+enum SCTextStyle {
+  // Heading[600]
+
+  // 2024-01-18 dev start
+
+  ///font_40px_w500_h100
+  font_40px_w500_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 40,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_32px_w500_h100
+  font_32px_w500_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 32,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_24px_w700_h100
+  font_24px_w700_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  /// font_20px_w700_h100
+  font_20px_w700_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_18px_w600_h100
+  font_18px_w600_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_16px_w600_h100
+  font_16px_w600_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_16px_w500_h100
+  font_16px_w500_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_16px_w400_h100
+  font_16px_w400_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_14px_w700_h100
+  font_14px_w700_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_14px_w500_h100
+  font_14px_w500_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_14px_w400_h120
+  font_14px_w400_h120(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 1.2,
+    color: Color(0xff000000),
+  )),
+
+  ///font_14px_w400_h100
+  font_14px_w400_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  ///font_12px_w600_h100
+  font_12px_w600_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    height: 1.0,
+    color: Color(0xff000000),
+  )),
+
+  // 2024-01-18 dev end
+
+  ///font-600-32px-100pc-P
+  font_600_32px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 32,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  /// font-600-24px-100pc-P
+  font_600_24px_100pc_P(
+    TextStyle(
+      fontFamily: 'Pretendard',
+      fontWeight: FontWeight.w600,
+      fontSize: 24,
+      height: 1.0,
+      color: Color(0xff444444),
+    ),
+  ),
+
+  /// font-600-20px-100pc-P
+  font_600_20px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  /// font-600-18px-100pc-P
+  font_600_18px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  /// font-600-16px-100pc-P
+  font_600_16px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-600-16px-140pc-P
+  font_600_16px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  /// font-600-14px-100pc-P
+  font_600_14px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-600-14px-140pc-P
+  font_600_14px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  /// font-600-13px-100pc-P
+  font_600_13px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-600-13px-140pc-P
+  font_600_13px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  /// font-600-12px-100pc-P
+  font_600_12px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-600-12px-140pc-P
+  font_600_12px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  //[500]
+
+  /// font-500-16px-100pc-P
+  font_500_16px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-500-16px-140pc-P
+  font_500_16px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  /// font-500-14px-100pc-P
+  font_500_14px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-500-14px-140pc-P
+  font_500_14px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  /// font-500-13px-100pc-P
+  font_500_13px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 13,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-500-13px-140pc-P
+  font_500_13px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 13,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  /// font-500-12px-100pc-P
+  font_500_12px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-500-12px-140pc-P
+  font_500_12px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  //[400]
+
+  /// font-400-16px-100pc-P
+  font_400_16px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-400-16px-140pc-P
+  font_400_16px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  /// font-400-14px-100pc-P
+  font_400_14px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  /// font-400-13px-100pc-P
+  font_400_13px_100pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  ///font-400-13px-140pc-P
+  font_400_13px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  ///font-400-12px-140pc-P
+  font_400_12px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  ///font-400-14px-140pc-P
+  font_400_14px_140pc_P(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 1.4,
+    color: Color(0xff444444),
+  )),
+
+  // Figma DesignSystem 추가
+  $font_40px_w500_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 40,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_32px_w500_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 32,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_24px_w700_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_20px_w700_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_18px_w600_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_16px_w600_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_16px_w500_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_16px_w400_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_14px_w700_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_14px_w500_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  // Figma DS에서 h120이랑 h100이랑 무슨 차이?
+  $font_14px_w400_h120(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  $font_14px_w400_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+
+  // 왜 w600인데 Figma DS에는 FontWeight.w700?
+  $font_12px_w600_h100(TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    height: 1.0,
+    color: Color(0xff444444),
+  )),
+  ;
+
+  const SCTextStyle(this._style);
+  final TextStyle _style;
+
+  TextStyle get value => _style;
+}
