98608352eae7419998548b8034248e93b7189d7b
8848man <83057037+8848man@users.noreply.github.com>
2024-01-22 11:37:23 +0900

    -- check out 커밋

--- lib/model/school_info.dart
+++ lib/model/school_info.dart
@@ -11,12 +11,12 @@ class SchoolInfoData with _$SchoolInfoData {
 @freezed
 class SchoolInfo with _$SchoolInfo {
   const factory SchoolInfo({
-    String? totalCount,
-    String? schoolName,
-    String? schoolGubun,
-    String? schoolType,
-    String? estType,
-    String? region,
-    String? link,
+    String? totalCount, // 데이터 총 갯수
+    String? schoolName, // 학교 이름
+    String? schoolGubun, // 학교 구분 (초,중,고 등)
+    String? schoolType, // 학교 타입 (4년제, 전문대 등)
+    String? estType, // 설립 유형 (사립,공립 등)
+    String? region, // 지역
+    String? link, // 학교 링크
   }) = _SchoolInfo;
 }
