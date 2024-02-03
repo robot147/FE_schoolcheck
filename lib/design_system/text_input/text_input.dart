f16dc506aad3187f6030d6bbc25f33906ad12ac4
8848man <83057037+8848man@users.noreply.github.com>
2024-02-04 00:05:50 +0900

    -- 텍스트인풋 수정

--- lib/design_system/text_input/text_input.dart
+++ lib/design_system/text_input/text_input.dart
@@ -184,11 +184,7 @@ class _SCTextInputState extends State<SCTextInput> {
                 Expanded(
                   child: widget._state == SCTextInputState.disabled
                       ? SCText(
-                          //widget.initValue ?? '',
-                          // 컨트롤러 설정 안할 경우 initValue 표기, initvalue 설정 안할 경우 '' 표기
-                          _controller != null
-                              ? widget.initValue ?? _controller!.text
-                              : widget.initValue ?? '',
+                          widget.initValue ?? '',
                           textStyle: SCTextStyle.font_14px_w400_h100,
                           color: SCColors.color_grey_50,
                         )
